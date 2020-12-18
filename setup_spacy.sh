3badb1755b6d718228138662037cb9e7e39190eb
raghavendra-musubi <raghavendra@techis.io>
2020-12-19 00:48:55 +0530

    first project completed for text summarization

--- setup_spacy.sh
+++ setup_spacy.sh
@@ -4,3 +4,15 @@ pip3 install -U spacy
 
 # install spacy english model
 python3 -m spacy download en
+
+# install large english model
+python3 -m spacy download en_core_web_lg
+
+# install numpy
+pip3 install numpy
+
+# install pandas 
+pip3 install pandas
+
+# install scikit-learn
+pip3 install -U scikit-learn
